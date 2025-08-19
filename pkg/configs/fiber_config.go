6cace7ba0d1a93511e159cdd3a0a0d2ebfe83199
Fadlan <fadlank.noer@gmail.com>
2025-08-19 21:49:26 +0700

    feat[Initialize]: godotenv Validate .env file

--- pkg/configs/fiber_config.go
+++ pkg/configs/fiber_config.go
@@ -6,17 +6,22 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
-	lib_zerolog "github.com/youtuber-setup-api/lib/zerolog"
+	"github.com/joho/godotenv"
+	"github.com/youtuber-setup-api/lib/zerolog"
 )
 
 // FiberConfig func for configuration Fiber app.
 // See: https://docs.gofiber.io/api/fiber#config
 func FiberConfig() fiber.Config {
+	// Check ENV File
+	if err := godotenv.Load(); err != nil {
+		zerolog.Logger().Panic().Msg("No ENV Loaded!")
+	}
+
 	// Define server settings.
-	readTimeoutSecondsCount, _ := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
-	if readTimeoutSecondsCount == 0 {
-		lib_zerolog.Logger().Panic().Msg("No ENV Loaded!")
-		panic("")
+	readTimeoutSecondsCount, err := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
+	if err != nil {
+		zerolog.Logger().Panic().Msg("Invald SERVER_READ_TIMEOUT configuration!")
 	}
 
 	// Return Fiber configuration.
